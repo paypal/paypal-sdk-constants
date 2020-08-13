4640e9cc7fa7f4ebc62252c80553f629221e89e9
Daniel Brain <dbrain@paypal.com>
2020-08-13 11:41:29 -0700

    Fix PayLaterEligibility to add recommended

--- src/types.js
+++ src/types.js
@@ -29,6 +29,7 @@ export type FundingProductEligibility = {|
 
 export type PayLaterEligibility = {|
     eligible : boolean,
+    recommended? : ?boolean,
     products? : {|
         payIn4 : FundingProductEligibility,
         flex : FundingProductEligibility
