f60ff6cf842ca50d0a06836245356e897a45d95a
Gabriel Ortega Rosero <gabrielo_9103@hotmail.com>
2021-04-06 14:08:13 -0500

    Add sandbox v2 environment variable (#50)

--- src/env.js
+++ src/env.js
@@ -4,6 +4,7 @@ export const ENV = {
     LOCAL:      ('local' : 'local'),
     STAGE:      ('stage' : 'stage'),
     SANDBOX:    ('sandbox' : 'sandbox'),
+    SANDBOX_V2: ('sandbox_v2' : 'sandbox_v2'),
     PRODUCTION: ('production' : 'production'),
     TEST:       ('test' : 'test')
 };
