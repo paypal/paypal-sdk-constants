d146d47bc27c49b88fdd912867cf06f962ec9c1b
Agnaldo Junior <agoliveira@paypal.com>
2021-06-02 16:02:53 -0400

    Add constant for validation_error
    Co-authored-by: Greg Jopa <534034+gregjopa@users.noreply.github.com>
    Signed-off-by: Agnaldo Junior <agoliveira@paypal.com>

---
+++ src/error.js
@@ -0,0 +1,5 @@
+/* @flow */
+
+export const ERROR_CODE = {
+    VALIDATION_ERROR: 'validation_error'
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 /* @flow */
 
-export * from './locale';
-export * from './params';
-export * from './env';
-export * from './fpti';
-export * from './order';
-export * from './platform';
-export * from './funding';
 export * from './defaults';
+export * from './env';
+export * from './error';
+export * from './fpti';
+export * from './funding';
+export * from './locale';
+export * from './order';
+export * from './params';
+export * from './platform';
 export * from './types';
