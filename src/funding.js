paypal/paypal-sdk-constants

commit dbfb307fa4e460741871bf56632e777d674488de

chore: Added Constant for PUI Funding Source (#92)
add Legal Component Constants

Co-authored-by: Ratnadeep Simhadri <rsimhadri@paypal.com>

src/funding.js
@@ -1,32 +1,33 @@
 /* @flow */
 
 export const FUNDING = {
-    PAYPAL:         ('paypal' : 'paypal'),
-    VENMO:          ('venmo' : 'venmo'),
-    APPLEPAY:       ('applepay' : 'applepay'),
-    ITAU:           ('itau' : 'itau'),
-    CREDIT:         ('credit' : 'credit'),
-    PAYLATER:       ('paylater' : 'paylater'),
-    CARD:           ('card' : 'card'),
-    IDEAL:          ('ideal' : 'ideal'),
-    SEPA:           ('sepa' : 'sepa'),
-    BANCONTACT:     ('bancontact' : 'bancontact'),
-    GIROPAY:        ('giropay' : 'giropay'),
-    SOFORT:         ('sofort' : 'sofort'),
-    EPS:            ('eps' : 'eps'),
-    MYBANK:         ('mybank' : 'mybank'),
-    P24:            ('p24' : 'p24'),
-    VERKKOPANKKI:   ('verkkopankki' : 'verkkopankki'),
-    PAYU:           ('payu' : 'payu'),
-    BLIK:           ('blik' : 'blik'),
-    TRUSTLY:        ('trustly' : 'trustly'),
-    ZIMPLER:        ('zimpler' : 'zimpler'),
-    MAXIMA:         ('maxima' : 'maxima'),
-    OXXO:           ('oxxo' : 'oxxo'),
-    BOLETO:         ('boleto' : 'boleto'),
-    WECHATPAY:      ('wechatpay' : 'wechatpay'),
-    MERCADOPAGO:    ('mercadopago' : 'mercadopago'),
-    MULTIBANCO:      ('multibanco' : 'multibanco')
+    PAYPAL:           ('paypal' : 'paypal'),
+    VENMO:            ('venmo' : 'venmo'),
+    APPLEPAY:         ('applepay' : 'applepay'),
+    ITAU:             ('itau' : 'itau'),
+    CREDIT:           ('credit' : 'credit'),
+    PAYLATER:         ('paylater' : 'paylater'),
+    CARD:             ('card' : 'card'),
+    IDEAL:            ('ideal' : 'ideal'),
+    SEPA:             ('sepa' : 'sepa'),
+    BANCONTACT:       ('bancontact' : 'bancontact'),
+    GIROPAY:          ('giropay' : 'giropay'),
+    SOFORT:           ('sofort' : 'sofort'),
+    EPS:              ('eps' : 'eps'),
+    MYBANK:           ('mybank' : 'mybank'),
+    P24:              ('p24' : 'p24'),
+    VERKKOPANKKI:     ('verkkopankki' : 'verkkopankki'),
+    PAYU:             ('payu' : 'payu'),
+    BLIK:             ('blik' : 'blik'),
+    TRUSTLY:          ('trustly' : 'trustly'),
+    ZIMPLER:          ('zimpler' : 'zimpler'),
+    MAXIMA:           ('maxima' : 'maxima'),
+    OXXO:             ('oxxo' : 'oxxo'),
+    BOLETO:           ('boleto' : 'boleto'),
+    WECHATPAY:        ('wechatpay' : 'wechatpay'),
+    MERCADOPAGO:      ('mercadopago' : 'mercadopago'),
+    MULTIBANCO:       ('multibanco' : 'multibanco'),
+    PAY_UPON_INVOICE: ('PayUponInvoice', 'PayUponInvoice')
 };
 
 export const FUNDING_BRAND_LABEL = {

commit 79a26d0aebb4a64813fb586ba089b668ad83aef8

chore: revert added constant for pui funding source (#92)" (#93)
This reverts commit dbfb307fa4e460741871bf56632e777d674488de, which caused a flow issue in checkout-components due to the way FUNDING is used/typed

src/funding.js
@@ -1,33 +1,32 @@
 /* @flow */
 
 export const FUNDING = {
-    PAYPAL:           ('paypal' : 'paypal'),
-    VENMO:            ('venmo' : 'venmo'),
-    APPLEPAY:         ('applepay' : 'applepay'),
-    ITAU:             ('itau' : 'itau'),
-    CREDIT:           ('credit' : 'credit'),
-    PAYLATER:         ('paylater' : 'paylater'),
-    CARD:             ('card' : 'card'),
-    IDEAL:            ('ideal' : 'ideal'),
-    SEPA:             ('sepa' : 'sepa'),
-    BANCONTACT:       ('bancontact' : 'bancontact'),
-    GIROPAY:          ('giropay' : 'giropay'),
-    SOFORT:           ('sofort' : 'sofort'),
-    EPS:              ('eps' : 'eps'),
-    MYBANK:           ('mybank' : 'mybank'),
-    P24:              ('p24' : 'p24'),
-    VERKKOPANKKI:     ('verkkopankki' : 'verkkopankki'),
-    PAYU:             ('payu' : 'payu'),
-    BLIK:             ('blik' : 'blik'),
-    TRUSTLY:          ('trustly' : 'trustly'),
-    ZIMPLER:          ('zimpler' : 'zimpler'),
-    MAXIMA:           ('maxima' : 'maxima'),
-    OXXO:             ('oxxo' : 'oxxo'),
-    BOLETO:           ('boleto' : 'boleto'),
-    WECHATPAY:        ('wechatpay' : 'wechatpay'),
-    MERCADOPAGO:      ('mercadopago' : 'mercadopago'),
-    MULTIBANCO:       ('multibanco' : 'multibanco'),
-    PAY_UPON_INVOICE: ('PayUponInvoice', 'PayUponInvoice')
+    PAYPAL:         ('paypal' : 'paypal'),
+    VENMO:          ('venmo' : 'venmo'),
+    APPLEPAY:       ('applepay' : 'applepay'),
+    ITAU:           ('itau' : 'itau'),
+    CREDIT:         ('credit' : 'credit'),
+    PAYLATER:       ('paylater' : 'paylater'),
+    CARD:           ('card' : 'card'),
+    IDEAL:          ('ideal' : 'ideal'),
+    SEPA:           ('sepa' : 'sepa'),
+    BANCONTACT:     ('bancontact' : 'bancontact'),
+    GIROPAY:        ('giropay' : 'giropay'),
+    SOFORT:         ('sofort' : 'sofort'),
+    EPS:            ('eps' : 'eps'),
+    MYBANK:         ('mybank' : 'mybank'),
+    P24:            ('p24' : 'p24'),
+    VERKKOPANKKI:   ('verkkopankki' : 'verkkopankki'),
+    PAYU:           ('payu' : 'payu'),
+    BLIK:           ('blik' : 'blik'),
+    TRUSTLY:        ('trustly' : 'trustly'),
+    ZIMPLER:        ('zimpler' : 'zimpler'),
+    MAXIMA:         ('maxima' : 'maxima'),
+    OXXO:           ('oxxo' : 'oxxo'),
+    BOLETO:         ('boleto' : 'boleto'),
+    WECHATPAY:      ('wechatpay' : 'wechatpay'),
+    MERCADOPAGO:    ('mercadopago' : 'mercadopago'),
+    MULTIBANCO:      ('multibanco' : 'multibanco')
 };
 
 export const FUNDING_BRAND_LABEL = {