1bd569129672b941bd0bf3f98c3a268f92913c54
Xuan Wan <xuanwan05@gmail.com>
2022-11-07 15:25:06 -0600

    simplify the code

--- src/params.ts
+++ src/params.ts
@@ -1,68 +1,68 @@
 export const SDK_PATH = "/sdk/js";
 export const SDK_SETTINGS = {
-  NAMESPACE: "data-namespace" as const,
-  CLIENT_TOKEN: "data-client-token" as const,
-  MERCHANT_ID: "data-merchant-id" as const,
-  PARTNER_ATTRIBUTION_ID: "data-partner-attribution-id" as const,
-  STAGE_HOST: "data-stage-host" as const,
-  API_STAGE_HOST: "data-api-stage-host" as const,
-  CSP_NONCE: "data-csp-nonce" as const,
-  ENABLE_3DS: "data-enable-3ds" as const,
-  SDK_INTEGRATION_SOURCE: "data-sdk-integration-source" as const,
-  USER_ID_TOKEN: "data-user-id-token" as const,
-  AMOUNT: "data-amount" as const,
-  CLIENT_METADATA_ID: "data-client-metadata-id" as const,
-  PAGE_TYPE: "data-page-type" as const,
-  USER_EXPERIENCE_FLOW: "data-user-experience-flow" as const,
-  POPUPS_DISABLED: "data-popups-disabled" as const,
-};
+  NAMESPACE: "data-namespace",
+  CLIENT_TOKEN: "data-client-token",
+  MERCHANT_ID: "data-merchant-id",
+  PARTNER_ATTRIBUTION_ID: "data-partner-attribution-id",
+  STAGE_HOST: "data-stage-host",
+  API_STAGE_HOST: "data-api-stage-host",
+  CSP_NONCE: "data-csp-nonce",
+  ENABLE_3DS: "data-enable-3ds",
+  SDK_INTEGRATION_SOURCE: "data-sdk-integration-source",
+  USER_ID_TOKEN: "data-user-id-token",
+  AMOUNT: "data-amount",
+  CLIENT_METADATA_ID: "data-client-metadata-id",
+  PAGE_TYPE: "data-page-type",
+  USER_EXPERIENCE_FLOW: "data-user-experience-flow",
+  POPUPS_DISABLED: "data-popups-disabled",
+} as const;
 export const SDK_QUERY_KEYS = {
-  COMPONENTS: "components" as const,
-  ENV: "env" as const,
-  DEBUG: "debug" as const,
-  CACHEBUST: "cachebust" as const,
-  CLIENT_ID: "client-id" as const,
-  MERCHANT_ID: "merchant-id" as const,
-  LOCALE: "locale" as const,
-  CURRENCY: "currency" as const,
-  INTENT: "intent" as const,
-  COMMIT: "commit" as const,
-  VAULT: "vault" as const,
-  BUYER_COUNTRY: "buyer-country" as const,
-  ENABLE_FUNDING: "enable-funding" as const,
-  DISABLE_FUNDING: "disable-funding" as const,
-  DISABLE_CARD: "disable-card" as const,
-  INTEGRATION_DATE: "integration-date" as const,
-  STAGE_HOST: "stage-host" as const,
-  STAGE_ALIAS: "stage-alias" as const,
-  CDN_REGISTRY: "cdn-registry" as const,
-  VERSION: "version" as const,
-};
+  COMPONENTS: "components",
+  ENV: "env",
+  DEBUG: "debug",
+  CACHEBUST: "cachebust",
+  CLIENT_ID: "client-id",
+  MERCHANT_ID: "merchant-id",
+  LOCALE: "locale",
+  CURRENCY: "currency",
+  INTENT: "intent",
+  COMMIT: "commit",
+  VAULT: "vault",
+  BUYER_COUNTRY: "buyer-country",
+  ENABLE_FUNDING: "enable-funding",
+  DISABLE_FUNDING: "disable-funding",
+  DISABLE_CARD: "disable-card",
+  INTEGRATION_DATE: "integration-date",
+  STAGE_HOST: "stage-host",
+  STAGE_ALIAS: "stage-alias",
+  CDN_REGISTRY: "cdn-registry",
+  VERSION: "version",
+} as const;
 export const COMPONENTS = {
-  BUTTONS: "buttons" as const,
-  HOSTED_FIELDS: "hosted-fields" as const,
-};
+  BUTTONS: "buttons",
+  HOSTED_FIELDS: "hosted-fields",
+} as const;
 export const DEBUG = {
-  TRUE: true as const,
-  FALSE: false as const,
-};
+  TRUE: true,
+  FALSE: false,
+} as const;
 export const QUERY_BOOL = {
-  TRUE: "true" as const,
-  FALSE: "false" as const,
-};
+  TRUE: "true",
+  FALSE: "false",
+} as const;
 export const UNKNOWN = "unknown" as const;
 export const PROTOCOL = {
-  HTTP: "http" as const,
-  HTTPS: "https" as const,
-};
+  HTTP: "http",
+  HTTPS: "https",
+} as const;
 export const PAGE_TYPES = {
-  HOME: "home" as const,
-  PRODUCT: "product" as const,
-  CART: "cart" as const,
-  CHECKOUT: "checkout" as const,
-  PRODUCT_LISTING: "product-listing" as const,
-  SEARCH_RESULTS: "search-results" as const,
-  PRODUCT_DETAILS: "product-details" as const,
-  MINI_CART: "mini-cart" as const,
-};
+  HOME: "home",
+  PRODUCT: "product",
+  CART: "cart",
+  CHECKOUT: "checkout",
+  PRODUCT_LISTING: "product-listing",
+  SEARCH_RESULTS: "search-results",
+  PRODUCT_DETAILS: "product-details",
+  MINI_CART: "mini-cart",
+} as const;
 export const MERCHANT_ID_MAX = 10;
