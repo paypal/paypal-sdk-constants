b096a110425cbfe205a04e92ef82218928dc270d
Xuan Wan <xuanwan05@gmail.com>
2022-11-07 15:25:06 -0600

    simplify the code

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,11 @@
 export const ENV = {
-  LOCAL: "local" as const,
-  STAGE: "stage" as const,
-  SANDBOX: "sandbox" as const,
-  PRODUCTION: "production" as const,
-  TEST: "test" as const,
-};
+  LOCAL: "local",
+  STAGE: "stage",
+  SANDBOX: "sandbox",
+  PRODUCTION: "production",
+  TEST: "test",
+} as const;
 export const MOBILE_ENV = {
-  ANDROID: "android" as const,
-  IOS: "iOS" as const,
-};
+  ANDROID: "android",
+  IOS: "iOS",
+} as const;
