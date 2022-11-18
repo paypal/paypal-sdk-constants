47f685f76beb4ff09d5c4d0bc0db47e1536bccea
Xuan Wan <xuanwan05@gmail.com>
2022-11-18 14:42:54 -0600

    Revert "replace const with enum"
    This reverts commit 9d5e402199ff07cac4fb4e78dd5ee1384ddf1e7b.

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,11 @@
-export enum ENV {
-  LOCAL = "local",
-  STAGE = "stage",
-  SANDBOX = "sandbox",
-  PRODUCTION = "production",
-  TEST = "test",
-}
-export enum MOBILE_ENV {
-  ANDROID = "android",
-  IOS = "iOS",
-}
+export const ENV = {
+  LOCAL: "local",
+  STAGE: "stage",
+  SANDBOX: "sandbox",
+  PRODUCTION: "production",
+  TEST: "test",
+} as const;
+export const MOBILE_ENV = {
+  ANDROID: "android",
+  IOS: "iOS",
+} as const;
