eab386044ba52126d3656e764a2c90b632a75c2c
Shane Brunson <brunson.ws@gmail.com>
2024-02-12 14:19:36 -0600

    metric namespace constant

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 /* @flow */
 
+export * from "./apm";
 export * from "./defaults";
 export * from "./env";
 export * from "./error";
 export * from "./fpti";
 export * from "./funding";
 export * from "./locale";
+export * from "./observability";
 export * from "./order";
 export * from "./params";
 export * from "./platform";
 export * from "./types";
-export * from "./apm";
